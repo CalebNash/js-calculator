44cd4c532f3c072a3bcf4e2b05bfd61e0cabdfb7
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-09-04 12:42:06 -0400

    all buttons alert

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,11 @@
-let $numberButtons = document.querySelectorAll("button.number");
+let $Buttons = document.querySelectorAll("button.operator, button.equal-sign, button.number, button.clear");
 
-function pushNumber(event) {
-  alert(event.target.value);
-}
 
-$numberButtons.forEach(button => {
-    button.addEventListener("click", pushNumber)
-});
-
-let $operatorButtons = document.querySelectorAll("button.operator");
 
 function pushOperator(event) {
   alert(event.target.value);
 }
 
-$operatorButtons.forEach(button => {
+$Buttons.forEach(button => {
     button.addEventListener("click", pushOperator)
 });
