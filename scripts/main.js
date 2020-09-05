8eca50e11ac81814c019e7fbbd3870ad08ea98d0
Caleb Nash <calebnash@Calebs-MBP.attlocal.net>
2020-09-05 15:24:18 -0400

    Im a web developer mode completed

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ let $operatorButtons = document.querySelectorAll("button.operator, button.clear"
 var calculation = [];
 
 function pushNumber(event) {
-  alert(event.target.value);
+   alert(event.target.value);
   let value = event.target.value;
   calculation.push(value);
   console.log(calculation);
@@ -16,10 +16,14 @@ $numberButtons.forEach(button => {
 });
 
 function pushOperator(event) {
-  alert(event.target.value);
+   alert(event.target.value);
   let value = event.target.value;
   calculation.push(value);
   console.log(calculation);
+
+  if(value === 'clear'){
+    calculation = [];
+  }
 }
 
 $operatorButtons.forEach(button => {
@@ -27,26 +31,37 @@ $operatorButtons.forEach(button => {
 });
 
 function pushEqual(event) {
-  alert(event.target.value);
+   alert(event.target.value);
   let fullNum1 = "";
   let fullNum2 = "";
   let operator = "";
+  let result;
 
   for(var i = 0; i < calculation.length; i++){
     if(calculation[i] === '+' || calculation[i] === '-' ||calculation[i] === '*' || calculation[i] === '\/'){
       operator = calculation[i];
     }
-  else if(operator){
-    fullNum2 = fullNum2 + calculation[i];
+    else if(operator){
+      fullNum2 = fullNum2 + calculation[i];
+    }
+    else {
+      fullNum1 = fullNum1 + calculation[i];
+    }
   }
-  else {
-    fullNum1 = fullNum1 + calculation[i];
+
+  if(operator === '+'){
+    result = parseInt(fullNum1) + parseInt(fullNum2);
   }
-    console.log(operator);
-    console.log(fullNum1);
-    console.log(fullNum2);
+  else if(operator === '-'){
+    result = parseInt(fullNum1) - parseInt(fullNum2);
   }
-  alert(operator);
-//console.log(fullNum1);
+  else if(operator === '*'){
+    result = parseInt(fullNum1) * parseInt(fullNum2);
+  }
+  else if(operator === '\/'){
+    result = parseInt(fullNum1) / parseInt(fullNum2);
+  }
+  alert(result);
+  calculation = [];
 }
   $equalButton.addEventListener("click", pushEqual);
