81df4e90e8543e28998bd4046a8c2dc3d9daf929
Caleb Nash <calebnash@Calebs-MBP.attlocal.net>
2020-09-07 14:23:30 -0400

    Im a web developer mode completed

--- scripts/main.js
+++ scripts/main.js
@@ -3,65 +3,63 @@ let $equalButton = document.querySelector("button.equal-sign");
 let $operatorButtons = document.querySelectorAll("button.operator, button.clear");
 var calculation = [];
 
+
 function pushNumber(event) {
-   alert(event.target.value);
+  //alert(event.target.value);
   let value = event.target.value;
   calculation.push(value);
   console.log(calculation);
-
 }
 
 $numberButtons.forEach(button => {
-    button.addEventListener("click", pushNumber)
+  button.addEventListener("click", pushNumber)
 });
 
 function pushOperator(event) {
-   alert(event.target.value);
+  //alert(event.target.value);
   let value = event.target.value;
   calculation.push(value);
   console.log(calculation);
 
-  if(value === 'clear'){
+  if (value === 'clear') {
     calculation = [];
   }
 }
 
 $operatorButtons.forEach(button => {
-    button.addEventListener("click", pushOperator)
+  button.addEventListener("click", pushOperator)
 });
 
 function pushEqual(event) {
-   alert(event.target.value);
+  //alert(event.target.value);
   let fullNum1 = "";
   let fullNum2 = "";
   let operator = "";
   let result;
 
-  for(var i = 0; i < calculation.length; i++){
-    if(calculation[i] === '+' || calculation[i] === '-' ||calculation[i] === '*' || calculation[i] === '\/'){
+  for (var i = 0; i < calculation.length; i++) {
+    if (calculation[i] === '+' || calculation[i] === '-' || calculation[i] === '*' || calculation[i] === '\/') {
       operator = calculation[i];
-    }
-    else if(operator){
+    } else if (operator) {
       fullNum2 = fullNum2 + calculation[i];
-    }
-    else {
+    } else {
       fullNum1 = fullNum1 + calculation[i];
     }
   }
 
-  if(operator === '+'){
+  if (operator === '+') {
     result = parseInt(fullNum1) + parseInt(fullNum2);
-  }
-  else if(operator === '-'){
+  } else if (operator === '-') {
     result = parseInt(fullNum1) - parseInt(fullNum2);
-  }
-  else if(operator === '*'){
+  } else if (operator === '*') {
     result = parseInt(fullNum1) * parseInt(fullNum2);
-  }
-  else if(operator === '\/'){
+  } else if (operator === '\/') {
     result = parseInt(fullNum1) / parseInt(fullNum2);
+  }else{
+    result = fullNum1;
   }
+
   alert(result);
   calculation = [];
 }
-  $equalButton.addEventListener("click", pushEqual);
+$equalButton.addEventListener("click", pushEqual);
